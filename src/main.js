2d76fc1badff76bbdaafa24d007aa7a3525e7190
lele <916577115@qq.com>
2018-07-30 10:53:18 +0800

    xiugai

--- src/main.js
+++ src/main.js
@@ -8,8 +8,9 @@ Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    //jfsdifjdsi 
+    router,
+    components: { App },
+    template: '<App/>'
 })
